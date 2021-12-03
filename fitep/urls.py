a835849a4dfd6ec09cdf1994a7197f302c667070
Hakan DEGIRMEN <degirmenhakan@gmail.com>
2021-12-04 00:03:11 +0300

    master added jwt auth

--- fitep/urls.py
+++ fitep/urls.py
@@ -20,10 +20,15 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from rest_framework_simplejwt import views as jwt_views
+
+
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/account/', include('account.api.urls')),
     path('api/branch/', include('branch.api.urls')),
+    path('api/token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
 ] + staticfiles_urlpatterns()
